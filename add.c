f9debc2d51e9e72eb071a1888f356ed65029f77a
mishal12344 <144669106+mishal12344@users.noreply.github.com>
2023-09-11 14:30:28 +0530

    my first commit

---
+++ add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int a,b,c;
+printf("enter the value for a:");
+scanf("%d",&a);
+printf("enter the value for b:");
+scanf("%d",&b);
+c=a+b;
+printf("result=%d",c);
+return 0;}
